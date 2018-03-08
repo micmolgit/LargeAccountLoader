b1890a99b0d20be92e83cc2a59f137eda7e9cc18
Michel Molongo <mic.mol@outlook.com>
2018-03-08 17:01:32 +0400

    Adding runnable .NetCore Application

---
+++ LargeAccountLoader/DcrmConnector.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Crm.Sdk.ServiceHelper;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+
+namespace LargeAccountLoader
+{
+    class DcrmConnector
+    {
+        private ServerConnection _serverConnection;
+        private ServerConnection.Configuration _serverConfig;
+        private OrganizationServiceProxy _serviceProxy;
+        private ServiceContext _serviceContext;
+
+        public ServiceContext SrvContext
+        {
+            get
+            {
+                if (_serviceContext == null)
+                    _serviceContext = new ServiceContext(_serviceProxy);
+
+                if (_serviceContext == null)
+                {
+                    throw new ArgumentNullException("ServiceContext", "ServiceContext Could not be created from OrganizationServiceProxy");
+                }
+                return _serviceContext;
+            }
+        }
+
+        public OrganizationServiceProxy ServiceProxy
+        {
+            get
+            {
+                return _serviceProxy;
+            }
+        }
+
+        public ServerConnection.Configuration ServiceConfig
+        {
+            get
+            {
+                return _serverConfig;
+            }
+        }
+
+        public void Connect()
+        {
+            // Obtain the target organization's Web address and client logon
+            // credentials from the user.
+            _serverConnection = new ServerConnection();
+            _serverConfig = _serverConnection.GetServerConfiguration();
+
+            _serviceProxy = new OrganizationServiceProxy(_serverConfig.OrganizationUri, _serverConfig.HomeRealmUri, _serverConfig.Credentials, _serverConfig.DeviceCredentials);
+
+            _serviceProxy.Authenticate();
+
+            // This statement is required to enable early-bound type support.
+            _serviceProxy.EnableProxyTypes();
+
+            if (!_serviceProxy.IsAuthenticated)
+            {
+                throw new InvalidOperationException("Authentication could not be completed");
+            }
+
+            Console.WriteLine($"Successfully connected to : {_serverConfig.OrganizationUri}");
+        }
+
+        public void Disconnect()
+        {
+            _serviceProxy?.Dispose();
+        }
+    }
+}
---
+++ LargeAccountLoader/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.ServiceModel;
+using Nito.AsyncEx;
+
+namespace LargeAccountLoader
+{
+    class Program
+    {
+        static public void Main(string[] args)
+        {
+            AsyncContext.Run(() => MainAsync(args));
+        }
+
+        static async void MainAsync(string[] args)
+        {
+            var largeAccountLoader = new LargeAccountLoader();
+
+            try
+            {
+                var accountGuid = "78E89CBF-AEB9-E611-80FC-005056A2D11D";
+                var cptEntities = await largeAccountLoader.Load(accountGuid);
+
+                Console.WriteLine($"{cptEntities} entities were found for {accountGuid}");
+            }
+            catch (FaultException<Microsoft.Xrm.Sdk.OrganizationServiceFault> ex)
+            {
+                Console.WriteLine("The application terminated with an error.");
+                Console.WriteLine($"Timestamp: { ex.Detail.Timestamp}");
+                Console.WriteLine($"Code: {ex.Detail.ErrorCode}");
+                Console.WriteLine($"Message: {ex.Detail.Message}");
+                Console.WriteLine($"Plugin Trace: {ex.Detail.TraceText}");
+                Console.WriteLine("Inner Fault: {0}",
+                    null == ex.Detail.InnerFault ? "No Inner Fault" : "Has Inner Fault");
+            }
+            catch (System.TimeoutException ex)
+            {
+                Console.WriteLine("The application terminated with an error.");
+                Console.WriteLine($"Message: {ex.Message}");
+                Console.WriteLine($"Stack Trace: {ex.StackTrace}");
+                Console.WriteLine("Inner Fault: {0}",
+                    null == ex.InnerException.Message ? "No Inner Fault" : ex.InnerException.Message);
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine("The application terminated with an error.");
+                Console.WriteLine(ex.Message);
+
+                // Display the details of the inner exception.
+                if (ex.InnerException != null)
+                {
+                    Console.WriteLine(ex.InnerException.Message);
+
+                    FaultException<Microsoft.Xrm.Sdk.OrganizationServiceFault> fe = ex.InnerException
+                        as FaultException<Microsoft.Xrm.Sdk.OrganizationServiceFault>;
+                    if (fe != null)
+                    {
+                        Console.WriteLine($"Timestamp: {fe.Detail.Timestamp}");
+                        Console.WriteLine($"Code: {fe.Detail.ErrorCode}");
+                        Console.WriteLine($"Message: {fe.Detail.Message}");
+                        Console.WriteLine($"Plugin Trace: {fe.Detail.TraceText}");
+                        Console.WriteLine("Inner Fault: {0}",
+                            null == fe.Detail.InnerFault ? "No Inner Fault" : "Has Inner Fault");
+                    }
+                }
+            }
+            finally
+            {
+                largeAccountLoader.Terminate();
+            }
+        }
+    }
+}
